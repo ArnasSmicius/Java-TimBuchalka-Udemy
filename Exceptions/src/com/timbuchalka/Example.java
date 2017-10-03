ecaf82689de0ffdf95ad5ecf8915463dfd11bf35
ArnasSmicius <arnasmic@gmail.com>
2017-10-03 21:20:54 +0300

    Exceptions stack trace

---
+++ Exceptions/src/com/timbuchalka/Example.java
@@ -0,0 +1,24 @@
+package com.timbuchalka;
+
+import java.util.Scanner;
+
+public class Example {
+
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+    }
+
+    private static int divide() {
+        int x = getInt();
+        int y = getInt();
+        System.out.println("x is " + x + ", y is " + y);
+        return x / y;
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        return s.nextInt();
+    }
+}

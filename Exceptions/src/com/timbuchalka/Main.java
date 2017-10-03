40d7a493564353dd15d4cd932e70c883f9c24df4
ArnasSmicius <arnasmic@gmail.com>
2017-10-03 20:53:24 +0300

    Exceptions. LBYL and EAFP methods

---
+++ Exceptions/src/com/timbuchalka/Main.java
@@ -0,0 +1,70 @@
+package com.timbuchalka;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(divideLBYL(x, y));
+//        System.out.println(divideEAFP(x, y));
+//        System.out.println(divide(x, y));
+        int x = getIntEAFP();
+        System.out.println("x is " + x);
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL() {
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer ");
+        String input = s.next();
+        for (int i = 0 ; i < input.length() ; i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid) {
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e) {
+            return 0;
+        }
+    }
+
+    private static int divideLBYL(int x, int y) {
+        if (y != 0) { // LBYL (Look before you leap) is checking that y != 0 before dividing
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int divideEAFP(int x, int y) { // EAFP (it's easier to ask for forgiveness than permission)
+                                                  // cathing exception after trying to divide
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    }
+
+    private static int divide(int x, int y) {
+        return x / y;
+    }
+}

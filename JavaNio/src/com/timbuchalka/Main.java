89a975c5e8b0f425eeb18d2908fd73f56d1fb09e
ArnasSmicius <arnasmic@gmail.com>
2017-10-31 19:19:42 +0200

    Chained put methods

--- JavaNio/src/com/timbuchalka/Main.java
+++ JavaNio/src/com/timbuchalka/Main.java
@@ -21,15 +21,44 @@ public class Main {
             FileChannel binChannel = binFile.getChannel()) {
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
+
+//            byte[] outputBytes = "Hello World!".getBytes();
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
+//            buffer.flip();
+
             byte[] outputBytes = "Hello World!".getBytes();
             buffer.put(outputBytes);
+            long int1Pos = outputBytes.length;
             buffer.putInt(245);
+            long int2Pos = int1Pos + Integer.BYTES;
             buffer.putInt(-98765);
             byte[] outputBytes2 = "Nice to meet you".getBytes();
             buffer.put(outputBytes2);
+            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
             buffer.putInt(1000);
             buffer.flip();
+
             binChannel.write(buffer);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte[] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new java.lang.String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte[] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new java.lang.String(inputString2));
+            System.out.println("int3 = " + readBuffer.getInt());
+
+
+
 //            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
 //            buffer.put(outputBytes);
 //

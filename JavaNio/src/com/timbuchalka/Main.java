b73a99fe6dc70595fd31ee807e05f93c536ca7ee
ArnasSmicius <arnasmic@gmail.com>
2017-11-05 11:10:53 +0200

    FileChannel to Copy Files and Pipes with Threads

--- JavaNio/src/com/timbuchalka/Main.java
+++ JavaNio/src/com/timbuchalka/Main.java
@@ -1,13 +1,12 @@
 package com.timbuchalka;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,163 +16,119 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try(FileOutputStream binFile = new FileOutputStream("data.dat");
-            FileChannel binChannel = binFile.getChannel()) {
 
-            ByteBuffer buffer = ByteBuffer.allocate(100);
+        try {
+            Pipe pipe = Pipe.open();
 
-//            byte[] outputBytes = "Hello World!".getBytes();
-//            byte[] outputBytes2 = "Nice to meet you".getBytes();
-//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
-//            buffer.flip();
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
 
-            byte[] outputBytes = "Hello World!".getBytes();
-            buffer.put(outputBytes);
-            long int1Pos = outputBytes.length;
-            buffer.putInt(245);
-            long int2Pos = int1Pos + Integer.BYTES;
-            buffer.putInt(-98765);
-            byte[] outputBytes2 = "Nice to meet you".getBytes();
-            buffer.put(outputBytes2);
-            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
-            buffer.putInt(1000);
-            buffer.flip();
+                        for(int i=0; i<10; i++) {
+                            String currentTime = "The time is: " + System.currentTimeMillis();
 
-            binChannel.write(buffer);
+                            buffer.put(currentTime.getBytes());
+                            buffer.flip();
 
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            FileChannel channel = ra.getChannel();
+                            while(buffer.hasRemaining()) {
+                                sinkChannel.write(buffer);
+                            }
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
 
-            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
-            channel.position(int3Pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
+            Runnable reader = new Runnable() {
+                @Override
+                public void run() {
+                     try {
+                         Pipe.SourceChannel sourceChannel = pipe.source();
+                         ByteBuffer buffer = ByteBuffer.allocate(56);
 
-            System.out.println("int3 = " + readBuffer.getInt());
-            readBuffer.flip();
-            channel.position(int2Pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
+                         for(int i=0; i<10; i++) {
+                             int bytesRead = sourceChannel.read(buffer);
+                             byte[] timeString = new byte[bytesRead];
+                             buffer.flip();
+                             buffer.get(timeString);
+                             System.out.println("Reader Thread: " + new String(timeString));
+                             buffer.flip();
+                             Thread.sleep(100);
+                         }
+                     } catch(Exception e) {
+                         e.printStackTrace();
+                     }
+                }
+            };
 
-            System.out.println("int2 = " + readBuffer.getInt());
-            readBuffer.flip();
-            channel.position(int1Pos);
-            channel.read(readBuffer);
-            readBuffer.flip();
+            new Thread(writer).start();
+            new Thread(reader).start();
 
-            System.out.println("int1 = " + readBuffer.getInt());
-
-            byte[] outputString = "Hello, World!".getBytes();
-            long str1Pos = 0;
-            long newInt1Pos = outputString.length;
-            long newInt2Pos = newInt1Pos + Integer.BYTES;
-            byte[] outputString2 = "Nice to meet you".getBytes();
-            long str2Pos = newInt2Pos + Integer.BYTES;
-            long newInt3Pos = str2Pos + outputString2.length;
-
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            binChannel.position(newInt1Pos);
-            binChannel.write(intBuffer);
-
-            intBuffer.flip();
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            binChannel.position(newInt2Pos);
-            binChannel.write(intBuffer);
-
-            intBuffer.flip();
-            intBuffer.putInt(1000);
-            intBuffer.flip();
-            binChannel.position(newInt3Pos);
-            binChannel.write(intBuffer);
-
-            binChannel.position(str1Pos);
-            binChannel.write(ByteBuffer.wrap(outputString));
-            binChannel.position(str2Pos);
-            binChannel.write(ByteBuffer.wrap(outputString2));
-
-//            ByteBuffer readBuffer = ByteBuffer.allocate(100);
-//            channel.read(readBuffer);
-//            readBuffer.flip();
-//            byte[] inputString = new byte[outputBytes.length];
-//            readBuffer.get(inputString);
-//            System.out.println("inputString = " + new java.lang.String(inputString));
-//            System.out.println("int1 = " + readBuffer.getInt());
-//            System.out.println("int2 = " + readBuffer.getInt());
-//            byte[] inputString2 = new byte[outputBytes2.length];
-//            readBuffer.get(inputString2);
-//            System.out.println("inputString2 = " + new java.lang.String(inputString2));
-//            System.out.println("int3 = " + readBuffer.getInt());
-
-
-
-//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
-//            buffer.put(outputBytes);
-//
-//            buffer.flip();
-//            int numBytes = binChannel.write(buffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-//            intBuffer.putInt(245);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            intBuffer.flip();
-//            intBuffer.putInt(-98765);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-//            FileChannel channel = ra.getChannel();
-//            outputBytes[0] = 'a';
-//            outputBytes[1] = 'b';
-//            buffer.flip();
-//            long numBytesRead = channel.read(buffer);
-//            if(buffer.hasArray()) {
-//                System.out.println("byte buffer = " + new java.lang.String(buffer.array()));
-////                System.out.println("byte buffer - " + new java.lang.String(outputBytes));
-//            }
-//
-//            // Absolute read
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            System.out.println(intBuffer.getInt(0));
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            intBuffer.flip();
-//            System.out.println(intBuffer.getInt(0));
-//            System.out.println(intBuffer.getInt());
-//
-//            // Relative read
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-//            channel.close();
-//            ra.close();
-////            System.out.println("outputBytes = " + new java.lang.String(outputBytes));
-//
-////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-////            byte[] b = new byte[outputBytes.length];
-////            ra.read(b);
-////            System.out.println(new java.lang.String(b));
-////
-////            long int1 = ra.readInt();
-////            long int2 = ra.readInt();
-////            System.out.println(int1);
-////            System.out.println(int2);
-//
         } catch(IOException e) {
             e.printStackTrace();
         }
+
+//        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+//            FileChannel binChannel = binFile.getChannel()) {
+//
+//            ByteBuffer buffer = ByteBuffer.allocate(100);
+//
+//            byte[] outputBytes = "Hello World!".getBytes();
+//            buffer.put(outputBytes);
+//            long int1Pos = outputBytes.length;
+//            buffer.putInt(245);
+//            long int2Pos = int1Pos + Integer.BYTES;
+//            buffer.putInt(-98765);
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes2);
+//            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
+//            buffer.putInt(1000);
+//            buffer.flip();
+//
+//            binChannel.write(buffer);
+//
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            FileChannel channel = ra.getChannel();
+//
+//            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            channel.position(int3Pos);
+//            channel.read(readBuffer);
+//            readBuffer.flip();
+//
+//            System.out.println("int3 = " + readBuffer.getInt());
+//            readBuffer.flip();
+//            channel.position(int2Pos);
+//            channel.read(readBuffer);
+//            readBuffer.flip();
+//
+//            System.out.println("int2 = " + readBuffer.getInt());
+//            readBuffer.flip();
+//            channel.position(int1Pos);
+//            channel.read(readBuffer);
+//            readBuffer.flip();
+//
+//            System.out.println("int1 = " + readBuffer.getInt());
+//
+//            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+//            FileChannel copyChannel = copyFile.getChannel();
+//            channel.position(0);
+////            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+//            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+//            System.out.println("Num transfered = " + numTransferred);
+//
+//            channel.close();
+//            ra.close();
+//            copyChannel.close();
+//
+//
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }

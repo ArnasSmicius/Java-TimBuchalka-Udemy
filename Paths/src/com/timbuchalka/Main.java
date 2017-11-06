5fbef7a96b5abee1d60c11272a5ff7beec0929ff
ArnasSmicius <arnasmic@gmail.com>
2017-11-06 21:59:24 +0200

    Filesystem in Java

---
+++ Paths/src/com/timbuchalka/Main.java
@@ -0,0 +1,32 @@
+package com.timbuchalka;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+        printFile(filePath);
+        filePath = Paths.get("/home/arnas/Documents/GitHub/Java-TimBuchalka-Udemy/OutThere.txt");
+        printFile(filePath);
+    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}

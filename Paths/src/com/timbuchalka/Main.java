59d0376c2748c2765598bf7951faecb875652c1d
ArnasSmicius <arnasmic@gmail.com>
2017-11-09 19:46:29 +0200

    More on Paths

--- Paths/src/com/timbuchalka/Main.java
+++ Paths/src/com/timbuchalka/Main.java
@@ -12,10 +12,18 @@ public class Main {
     public static void main(String[] args) {
         Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
-        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
         printFile(filePath);
         filePath = Paths.get("/home/arnas/Documents/GitHub/Java-TimBuchalka-Udemy/OutThere.txt");
         printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
     }
 
     private static void printFile(Path path) {

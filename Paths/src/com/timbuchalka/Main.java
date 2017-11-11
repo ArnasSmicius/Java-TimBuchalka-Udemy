4e29bbadc587ff43ca00091be92ceaaeb227f616
ArnasSmicius <arnasmic@gmail.com>
2017-11-11 17:59:58 +0200

    Checking that file exists and Copying the file

--- Paths/src/com/timbuchalka/Main.java
+++ Paths/src/com/timbuchalka/Main.java
@@ -10,31 +10,50 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-//        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
-        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
-        printFile(filePath);
-        filePath = Paths.get("/home/arnas/Documents/GitHub/Java-TimBuchalka-Udemy/OutThere.txt");
-        printFile(filePath);
 
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
-
-        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath());
-        printFile(path2.normalize());
-    }
-
-    private static void printFile(Path path) {
-        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
-            String line;
-            while((line = fileReader.readLine()) != null) {
-                System.out.println(line);
-            }
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
             e.printStackTrace();
         }
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+////        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".", "files", "SubdirectoryFile.txt");
+//        printFile(filePath);
+//        filePath = Paths.get("/home/arnas/Documents/GitHub/Java-TimBuchalka-Udemy/OutThere.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisfiledoentexist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("/home/Test/ING", "abcdef", "whatever.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//
+//        System.out.println("Exists = " + Files.exists(path4));
+//    }
+//
+//    private static void printFile(Path path) {
+//        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
     }
 }

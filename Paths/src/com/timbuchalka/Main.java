1deaa2d395a57564b65b7dee222208e663e7d7cf
ArnasSmicius <arnasmic@gmail.com>
2017-11-13 21:19:40 +0200

    Moving, Renaming and Deleting files

--- Paths/src/com/timbuchalka/Main.java
+++ Paths/src/com/timbuchalka/Main.java
@@ -2,21 +2,32 @@ package com.timbuchalka;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 //        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
 //        printFile(path);

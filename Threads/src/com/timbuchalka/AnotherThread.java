48f8160adc97bbe49ec661638ae7a9b6637346e2
ArnasSmicius <arnasmic@gmail.com>
2017-12-14 19:21:35 +0200

    Interrupt and Join

--- Threads/src/com/timbuchalka/AnotherThread.java
+++ Threads/src/com/timbuchalka/AnotherThread.java
@@ -11,6 +11,7 @@ public class AnotherThread extends Thread {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             System.out.println(ANSI_BLUE + "Another thread woke me up");
+            return;
         }
 
         System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
--- Threads/src/com/timbuchalka/Main.java
+++ Threads/src/com/timbuchalka/Main.java
@@ -24,6 +24,12 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println("I couldn't wait after all. I was interrupted");
+                }
             }
         });
         myRunnableThread.start();

244ae73535172e213eadc24708a24d822d941675
ArnasSmicius <arnasmic@gmail.com>
2017-12-17 18:12:35 +0200

    Multiple Threads

---
+++ Counter/src/com/timbuchalka/Main.java
@@ -0,0 +1,51 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Countdown {
+    private int i;
+    public void doCountdown() {
+        String color;
+
+        switch(Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+        }
+
+        for(i=10; i > 0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    @Override
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}

b484b6d1675fe9526a2be72582280feb9a330528
arnasmic@gmail.com <arnasmic@gmail.com>
2017-12-22 23:03:04 +0200

    Adventure Game Challenge

---
+++ AdventureGameChallenge/src/com/timbuchalka/Location.java
@@ -0,0 +1,35 @@
+package com.timbuchalka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dev on 8/12/2015.
+ */
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+        this.exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}

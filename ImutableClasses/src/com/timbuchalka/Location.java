4393ab4b6bef317f45fa6cf94b6cfc8fb5f8d7b7
arnasmic@gmail.com <arnasmic@gmail.com>
2017-12-23 20:27:49 +0200

    Imutable Classes

---
+++ ImutableClasses/src/com/timbuchalka/Location.java
@@ -0,0 +1,36 @@
+package com.timbuchalka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dev on 8/12/2015.
+ */
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>(exits);
+        this.exits.put("Q", 0);
+    }
+
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}

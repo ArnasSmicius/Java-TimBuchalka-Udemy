ed955f8fc6d167a64e6d39bcd004949fc5003daf
ArnasSmicius <arnasmic@gmail.com>
2018-01-06 12:19:06 +0200

    Eliminating Thread interference

--- ProducerConsumer/src/com/timbuchalka/Main.java
+++ ProducerConsumer/src/com/timbuchalka/Main.java
@@ -37,8 +37,9 @@ class MyProducer implements Runnable {
         for (String num: nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
-
+                synchronized (buffer) {
+                    buffer.add(num);
+                }
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
@@ -46,7 +47,9 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting...");
-        buffer.add("EOF");
+        synchronized (buffer) {
+            buffer.add("EOF");
+        }
     }
 }
 
@@ -61,14 +64,16 @@ class MyConsumer implements Runnable {
 
     public void run() {
         while (true) {
-            if (buffer.isEmpty()) {
-                continue;
-            }
-            if(buffer.get(0).equals(EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if(buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
         }
     }

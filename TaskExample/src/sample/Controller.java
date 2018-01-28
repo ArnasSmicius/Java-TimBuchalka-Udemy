6c4e8ede05ec8d9f30ee79d420e53ab3ebb66bbf
ArnasSmicius <arnasmic@gmail.com>
2018-01-28 20:08:05 +0200

    Data Binding in JavaFX and added ProgressBar and ProgressLabel to the application

--- TaskExample/src/sample/Controller.java
+++ TaskExample/src/sample/Controller.java
@@ -5,7 +5,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
@@ -14,25 +16,42 @@ public class Controller {
     @FXML
     private ListView listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
     public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
 
-                ObservableList<String> employees = FXCollections.observableArrayList(
-                        "Tim Buchalka",
+                String[] names = {"Tim Buchalka",
                         "Bil Rogers",
                         "Jack Jill",
                         "Joan Andrews",
                         "Mary Johnson",
-                        "Bob McDonald");
+                        "Bob McDonald"};
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for (int i = 0; i < names.length; i++) {
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, names.length);
+                    Thread.sleep(200);
+                }
 
                 return employees;
             }
         };
 
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
+
     }
 
     @FXML

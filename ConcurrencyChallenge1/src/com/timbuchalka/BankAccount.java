b365b7f7f7c98156a951df1e4177f7a22c33329b
ArnasSmicius <arnasmic@gmail.com>
2018-02-02 20:52:20 +0200

    Thread Challenge 3, 4 and 6

--- ConcurrencyChallenge1/src/com/timbuchalka/BankAccount.java
+++ ConcurrencyChallenge1/src/com/timbuchalka/BankAccount.java
@@ -1,25 +1,59 @@
 package com.timbuchalka;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        lock = new ReentrantLock();
     }
 
     public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+
         }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+
         }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account number = " + accountNumber);
     }
 
     public double getBalance() {

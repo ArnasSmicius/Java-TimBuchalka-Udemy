dd8ab07058f3e4bf52ba5e72bbd46a831bc4a755
ArnasSmicius <arnasmic@gmail.com>
2018-02-14 20:35:56 +0200

    Thread challenges completed!

---
+++ ConcurrencyChallenge1/src/com/timbuchalka/Challenge8.java
@@ -0,0 +1,72 @@
+package com.timbuchalka;
+
+public class Challenge8 {
+
+    public static void main(String[] args) {
+        Tutor tutor = new Tutor();
+        Student student = new Student(tutor);
+        tutor.setStudent(student);
+
+        Thread tutorThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                tutor.studyTime();
+            }
+        });
+
+        Thread studentThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                student.handInAssignment();
+            }
+        });
+
+        tutorThread.start();
+        studentThread.start();
+    }
+}
+
+class Tutor {
+    private Student student;
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void studyTime() {
+        System.out.println("Tutor has arrived");
+        try {
+            // wait for student to arrive and hand in assignment
+            Thread.sleep(300);
+        }
+        catch (InterruptedException e) {
+
+        }
+        student.startStudy();
+        System.out.println("Tutor is studying with student");
+    }
+
+    public void getProgressReport() {
+        // get progress report
+        System.out.println("Tutor gave progress report");
+    }
+}
+
+class Student {
+
+    private Tutor tutor;
+
+    Student(Tutor tutor) {
+        this.tutor = tutor;
+    }
+
+    public void startStudy() {
+        // study
+        System.out.println("Student is studying");
+    }
+
+    public void handInAssignment() {
+        tutor.getProgressReport();
+        System.out.println("Student handed in assignment");
+    }
+}
